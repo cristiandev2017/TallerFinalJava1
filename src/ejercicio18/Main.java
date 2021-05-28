67e5513284a4ccfc3d5332efb0f4f3b761c1ee6b
Cristian <camilo.santamaria322@gmail.com>
2021-05-28 10:32:30 -0500

    Se crean las 5 instancias de Series y se prueba su funcionamiento

--- src/ejercicio18/Main.java
+++ src/ejercicio18/Main.java
@@ -1,4 +1,17 @@
 package ejercicio18;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Serie[] series = new Serie[5];
+
+        series[0] = new Serie("Lol", 11,"Rol", "Anton");
+        series[1] = new Serie("Escorpion", 8,"Drama", "Pepe");
+        series[2] = new Serie("Pedro", 9,"Accion", "sancho");
+        series[3] = new Serie("Pan", 11,"Drama", "Paco");
+        series[4] = new Serie("Paquito", 14,"Rol", "Anton");
+
+        series[0].entregar();
+        System.out.println(series[0].isEntregado());
+    }
 }
